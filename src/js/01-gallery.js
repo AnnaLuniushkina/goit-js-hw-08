ed00ceb5d7a4274d088c418bdab2bc4917cd38fa
AnnaLuniushkina <anna.luniushkina@gmail.com>
2022-04-18 15:45:31 +0300

    add gallery and video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const listItemsMarkup = createGalleryElMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", listItemsMarkup);
+
+function createGalleryElMarkup(galleryItems) {
+    return galleryItems.map(galleryItem => `<a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`).join('');
+}
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {    
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    event.preventDefault();
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+const Player = require("@vimeo/player");
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+    player.on('timeupdate', throttle(function (evt) {
+        console.log(evt.seconds);
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(evt.seconds));
+    }, 1000));
+
+    if (localStorage.getItem('videoplayer-current-time')) {
+    player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time'))).then(function (seconds) {
+    console.log(`Час відтворення відео із localeStorage: ${seconds}`);
+})
+    .catch(function (error) {
+    switch (error.name) {
+    case 'RangeError':
+    console.log('Час був менше 0 або більше, ніж тривалість відео');
+    break;
+
+    default:
+    console.log('помилки викликані іншими причинами');
+    break;
+}
+});
+}

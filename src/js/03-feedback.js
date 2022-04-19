5eb9beeed247d950b8e49562d6d3dadf9e882cd2
AnnaLuniushkina <anna.luniushkina@gmail.com>
2022-04-19 20:34:12 +0300

    add feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const throttle = require('lodash.throttle');
 
+const formRefs = document.querySelector('.feedback-form');
+
+let formData = {};
+
+formRefs.addEventListener('submit', onFormSubmit);
+formRefs.addEventListener('input', throttle(onFormInput, 500));
+
+savedForm();
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    console.log(formData);
+
+    if (formData) {
+        localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    }
+}
+
+function savedForm() {
+    let formData = localStorage.getItem('feedback-form-state');
+
+    if (formData) {
+        formData = JSON.parse(formData);
+        console.log(formData);
+    };
+
+    Object.entries(formData).forEach(
+        ([name, value]) => (formRefs.elements[name].value = value),
+    );
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log('Відправляємо форму');
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+    console.log(formData);
+}
